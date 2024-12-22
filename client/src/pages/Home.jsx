177c770c4d15808522b1f0468819ef97312852e3
Slavi Hristev <Slavihristev97@gmail.com>
2024-12-22 12:45:36 +0200

    Started working on the home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,23 @@
+import {Link} from 'react-router-dom'
+
 const Home = () =>{
 
     return(
-        <div className="">Home</div>
+        <div className="mt-4 flex flex-col gap-4">
+            <div className="flex gap-4">
+                <Link to='/'/>
+                <span className='text-blue-800'>Blogs and Articles</span>
+            </div>
+            <div className="flex items-center justify-between">
+                <div>
+                    <h1 className='text-gray-800 text-2xl md:text-5xl lg:text-6xl font-bold'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quae, facilis.</h1>
+                    <p className='mt-8 text-md md:text-xl'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt expedita eaque explicabo dolores repudiandae doloremque.</p>
+                </div>
+                <Link to='write'>
+                    
+                </Link>
+            </div>
+        </div>
     )
 }
 

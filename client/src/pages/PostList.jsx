b19f03d8bc59e30ca51fd4733871a89a273d2cbb
Slavi Hristev <Slavihristev97@gmail.com>
2024-12-31 12:30:16 +0200

    Created Postlists page

--- client/src/pages/PostList.jsx
+++ client/src/pages/PostList.jsx
@@ -1,8 +1,29 @@
-const PostList = () =>{
+import { useState } from "react";
+import PostList from "../components/PostList";
+import SideMenu from "../components/SideMenu";
 
-    return(
-        <div className="">PostList</div>
-    )
-}
+const PostListPage = () => {
+  const [open, setOpen] = useState(false);
 
-export default PostList;
+  return (
+    <div className="">
+      <h1 className="mb-8 text-2xl">Development Blog</h1>
+      <button
+        onClick={() => setOpen((prev) => !prev)}
+        className="md-hidden bg-blue-800 text-sm text-white px-4 py-2 rounded-2xl mb-4"
+      >
+        {open ? "Close" : "Filter or Search"}
+      </button>
+      <div className="flex flex-col-reverse gap-8 md:flex-row">
+        <div>
+          <PostList />
+        </div>
+        <div className={`${open ? "block" : "hidden"} md:block`}>
+          <SideMenu />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PostListPage;

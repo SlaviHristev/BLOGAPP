6d15d8ea4fca496c1ed4b6580b6c20bc096ead3f
Slavi Hristev <Slavihristev97@gmail.com>
2025-01-02 18:04:11 +0200

    Added user, post and comment models and made post controller

--- api/index.js
+++ api/index.js
@@ -4,10 +4,22 @@ import commentsRouter from './routes/comment.route.js'
 import postsRouter from './routes/post.route.js'
 import connectDb from './lib/connectDb.js'
 const app =express();
+app.use(express.json())
 
 app.use('/users', userRouter);
 app.use('/posts', postsRouter);
 app.use('/comments', commentsRouter);
+
+app.use((error, req,res,next)=>{
+
+    res.status(error.status || 500)
+
+    res.json({
+        message:error.message || "Something went wrong!",
+        status:error.status,
+        stack:error.stack
+    })
+})
 
 app.listen(3000, () =>{
     connectDb();

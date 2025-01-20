39759848648cfe352671355a916a1b9e2b555aa8
Slavi Hristev <Slavihristev97@gmail.com>
2025-01-20 18:00:02 +0200

    Added deleting and saving posts

--- client/src/components/PostMenuActions.jsx
+++ client/src/components/PostMenuActions.jsx
@@ -1,10 +1,13 @@
 import { useAuth, useUser } from "@clerk/clerk-react";
-import { useMutation, useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 const PostMenuActions = ({ post }) => {
   const { user } = useUser();
   const { getToken } = useAuth();
+  const navigate = useNavigate();
 
   const {
     isPending,
@@ -22,6 +25,55 @@ const PostMenuActions = ({ post }) => {
     },
   });
 
+  const deleteMutation = useMutation({
+    mutationFn: async () => {
+      const token = await getToken();
+      return axios.delete(`${import.meta.env.VITE_API_URL}/posts/${post._id}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+    },
+    onSuccess: () => {
+      toast.success("Post deleted successfully!");
+      navigate("/");
+    },
+    onError: (error) => {
+      toast.error(error.response.data);
+    },
+  });
+
+  const queryClient = useQueryClient();
+
+  const saveMutation = useMutation({
+    mutationFn: async () => {
+      const token = await getToken();
+      return axios.patch(`${import.meta.env.VITE_API_URL}/users/save`,{
+        postId: post._id,
+      }, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({queryKey:['savedPosts']})
+    },
+    onError: (error) => {
+      toast.error(error.response.data);
+    },
+  });
+
+  const handleDelete = () => {
+    deleteMutation.mutate();
+  };
+
+  const handleSave = () => {
+    if(!user){
+      return navigate('/login');
+    }
+    saveMutation.mutate();
+  };
 
   const isSaved = savedPosts?.data?.some((p) => p === post._id) || false;
   return (
@@ -32,7 +84,7 @@ const PostMenuActions = ({ post }) => {
       ) : error ? (
         "Saved posts fetching failed!"
       ) : (
-        <div className="flex items-center gap-2 py-2 text-sm cursor-pointer">
+        <div className="flex items-center gap-2 py-2 text-sm cursor-pointer" onClick={handleSave}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 48 48"
@@ -43,14 +95,18 @@ const PostMenuActions = ({ post }) => {
               d="M12 4C10.3 4 9 5.3 9 7v34l15-9 15 9V7c0-1.7-1.3-3-3-3H12z"
               stroke="black"
               strokeWidth="2"
-              fill={isSaved ? "black" : "none"}
+              fill= {saveMutation.isPending ? isSaved ? "none" : "black" : isSaved ? "black" : "none"}
             />
           </svg>
           <span>Save this Post</span>
+          {saveMutation.isPending && <span className="text-xs">(in progress)</span>}
         </div>
       )}
       {user && post.user.username === user.username && (
-        <div className="flex items-center gap-2 py-2 text-sm cursor-pointer">
+        <div
+          className="flex items-center gap-2 py-2 text-sm cursor-pointer"
+          onClick={handleDelete}
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 48 48"
@@ -61,9 +117,11 @@ const PostMenuActions = ({ post }) => {
               d="M24 2L29.39 16.26L44 18.18L33 29.24L35.82 44L24 37L12.18 44L15 29.24L4 18.18L18.61 16.26L24 2Z"
               stroke="black"
               strokeWidth="2"
+             
             />
           </svg>
           <span>Delete this Post</span>
+          {deleteMutation.isPending && <span className="text-xs">(in progress)</span>}
         </div>
       )}
     </div>

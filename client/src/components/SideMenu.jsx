92c038238af7a1640be4d408c7cdddf8b9fc05d6
Slavi Hristev <Slavihristev97@gmail.com>
2025-01-24 17:32:57 +0200

    Can now sort by clicking on the options on the side menu and increasing visit number when visiting a single page

--- client/src/components/SideMenu.jsx
+++ client/src/components/SideMenu.jsx
@@ -1,7 +1,17 @@
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 import Search from "./Search";
 
 const SideMenu = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const handleFilterChange = (e) => {
+    if (searchParams.get("sort") !== e.target.value) {
+      setSearchParams({
+        ...Object.fromEntries(searchParams.entries()),
+        sort: e.target.value,
+      });
+    }
+  };
   return (
     <div className="px-4 h-max sticky top-8">
       <h1 className="mb-4 text-sm font-medium">Search</h1>
@@ -11,7 +21,7 @@ const SideMenu = () => {
         <label htmlFor="" className="flex items-center gap-2 cursor-pointer">
           <input
             type="radio"
-            name="sort"
+            onChange={handleFilterChange}
             value="newest"
             className="appearance-none w-4 h-4 border-[1.5px] border-blue-800 rounded-sm bg-white checked:bg-blue-800"
           />
@@ -20,7 +30,7 @@ const SideMenu = () => {
         <label htmlFor="" className="flex items-center gap-2 cursor-pointer">
           <input
             type="radio"
-            name="sort"
+            onChange={handleFilterChange}
             value="popular"
             className="appearance-none w-4 h-4 border-[1.5px] border-blue-800 rounded-sm bg-white checked:bg-blue-800 "
           />
@@ -29,7 +39,7 @@ const SideMenu = () => {
         <label htmlFor="" className="flex items-center gap-2 cursor-pointer">
           <input
             type="radio"
-            name="sort"
+            onChange={handleFilterChange}
             value="trending"
             className="appearance-none w-4 h-4 border-[1.5px] border-blue-800 rounded-sm bg-white checked:bg-blue-800"
           />
@@ -38,7 +48,7 @@ const SideMenu = () => {
         <label htmlFor="" className="flex items-center gap-2 cursor-pointer">
           <input
             type="radio"
-            name="sort"
+            onChange={handleFilterChange}
             value="oldest"
             className="appearance-none w-4 h-4 border-[1.5px] border-blue-800 rounded-sm bg-white checked:bg-blue-800"
           />
